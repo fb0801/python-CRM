9943d226af66006ab6f4d8a2af65b4d0a1c559cb
fb0801 <fbhatti0801@gmail.com>
2023-10-19 18:20:13 +0100

    login users

--- myworld/dcrm/website/views.py
+++ myworld/dcrm/website/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+    #check if log in
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        #auth
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You have logged in')
+            return redirect('home')
+        else:
+            messages.success(request, 'Error occured when loggin in please try again or signup')
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
+
+def login_user(request):
+    pass
+
+def logout_user(request):
+    pass
